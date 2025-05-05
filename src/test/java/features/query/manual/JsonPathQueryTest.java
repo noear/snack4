982534cfd8907f682a2fc137035513d9bf309841
noear <noear@live.cn>
2025-05-05 13:02:47 +0800

    添加 jsonpath 单测

--- src/test/java/features/query/manual/JsonPathQueryTest.java
+++ src/test/java/features/query/manual/JsonPathQueryTest.java
@@ -28,7 +28,7 @@ public class JsonPathQueryTest {
     }
 
     @Test
-    public void getArrayDotTest() throws Exception {
+    public void getArrayMoreIndexTest() throws Exception {
         ONode n = ONode.loadJson("{code:1,msg:'Hello world',data:{list:[1,2,3,4,5], ary2:[{a:2},{a:3,b:{c:'ddd'}}]}}");
 
         ONode tmp = n.select("$.data.list[1,4]");
@@ -37,7 +37,7 @@ public class JsonPathQueryTest {
     }
 
     @Test
-    public void getArrayRangeTest(){
+    public void getArrayRangeTest() {
         ONode n = ONode.loadJson("{code:1,msg:'Hello world',data:{list:[1,2,3,4,5], ary2:[{a:2},{a:3,b:{c:'ddd'}}]}}");
 
         ONode tmp = n.select("$.data.list[1:4]");
@@ -52,5 +52,19 @@ public class JsonPathQueryTest {
         ONode tmp = n.select("$.data.list[?(@ in $..ary2[0].a)]");
         assert tmp.size() == 1;
         assertEquals(1, tmp.size());
+    }
+
+    @Test
+    public void getAllAndPropTest() throws Exception {
+        String json = "{\"store\":{\"book\":[{\"category\":\"reference\",\"author\":\"Nigel Rees\",\"title\":\"Sayings of the Century\",\"price\":8.95},{\"category\":\"fiction\",\"author\":\"Evelyn Waugh\",\"title\":\"Sword of Honour\",\"price\":12.99,\"isbn\":\"0-553-21311-3\"}],\"bicycle\":{\"color\":\"red\",\"price\":19.95}}}";
+        ONode n = ONode.loadJson(json);
+
+        ONode t7 = n.select("$..book[*].author");
+        System.out.println(t7);
+        assert t7.isArray() && t7.size() == 2;
+
+        ONode t8 = n.select("$..book.author");
+        System.out.println(t8);
+        assert t8.isArray() && t8.size() == 0;
     }
 }
