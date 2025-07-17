c056d81c427a5466a3be51cdd17fe25f05a352e5
noear <noear@live.cn>
2025-07-17 16:48:22 +0800

    添加测试

---
+++ src/test/java/benchmark/load/_models/UserModel.java
@@ -0,0 +1,16 @@
+package benchmark.load._models;
+
+public class UserModel {
+    public int id;
+    public String name;
+    public String note;
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", note='" + note + '\'' +
+                '}';
+    }
+}
